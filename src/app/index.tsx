4f1f72142007abe3e40a0463a0b0b41ecfb8b028
Marcondezz <igorpmarcondes@yahoo.com>
2024-08-29 12:01:15 -0300

    Construção

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import Header from "./Componentes/Header";
 import Footer from "./Componentes/Footer";
-import Paginainicial from "./Componentes/Desenvolvedores"
+import Paginainicial from "./Componentes/Pagina Inicial/PaginaInicial"
 
 export default function index(){
     return(
