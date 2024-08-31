a71fd44a9b20620f9713f0e99437d5fb77c54a37
Marcondezz <igorpmarcondes@yahoo.com>
2024-08-31 19:13:22 -0300

    [ Construção da página de personagens ]

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import Header from "./Componentes/Header";
 import Footer from "./Componentes/Footer";
-import Personagem from "./Componentes/personagens"
+import Personagem from "./Componentes/Pagina Inicial/PaginaInicial"
 
 export default function index(){
     return(
